63b2716edc65e6220ece4824bc3fb40a5a3faf98
Alexander Bet <ibetram12@gmail.com>
2025-08-13 00:44:06 +0300

    init: initial project

--- js/burger-menu/burger-menu.js
+++ js/burger-menu/burger-menu.js
@@ -1,4 +1,4 @@
-const burgerMenuButtonElement = document.querySelector('.burger__menu-button')
+const burgerMenuButtonElement = document.querySelector('.burger-btn')
 const menuElement = document.querySelector('.header__menu')
 const menuLinkElements = document.querySelectorAll('.header__menu-link')
 const bodyElement = document.body
@@ -12,14 +12,14 @@ if (burgerMenuButtonElement) {
 }
 
 function burgerMenuActive() {
-   burgerMenuButtonElement.classList.toggle('_active-burger')
-   menuElement.classList.toggle('_open-menu')
+   burgerMenuButtonElement.classList.toggle('_active-btn')
+   menuElement.classList.toggle('_menu-open')
    bodyElement.classList.toggle('_scroll-hidden')
 }
 
 function removeBurgerMenu() {
-   burgerMenuButtonElement.classList.remove('_active-burger')
-   menuElement.classList.remove('_open-menu')
+   burgerMenuButtonElement.classList.remove('_active-btn')
+   menuElement.classList.remove('_menu-open')
    bodyElement.classList.remove('_scroll-hidden')
 }
 
