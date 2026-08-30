cca43ee50d8ba61ab3a6777c5a67255611cb73ac
Alexander Bet <ibetram12@gmail.com>
2026-08-31 01:48:55 +0300

    fix: remove link path with # from search bar & add canonical link for website

--- src/assets/scripts/header_link-active/header__click-active.js
+++ src/assets/scripts/header_link-active/header__click-active.js
@@ -1,7 +1,6 @@
 const headerLinkElements = document.querySelectorAll('.header__menu-link')
 
-headerLinkElements.forEach((link) => {
-
+headerLinkElements.forEach(link => {
    link.addEventListener('click', () => {
       headerLinkElements.forEach((link) => {
          link.classList.remove('_active-link')
@@ -9,6 +8,22 @@ headerLinkElements.forEach((link) => {
 
       link.classList.add('_active-link')
    })
+
+   link.addEventListener('click', function (e) {
+      const targetId = this.getAttribute('href')
+
+      if (targetId && targetId.startsWith('#') && targetId.length > 1) {
+         e.preventDefault()
+
+         const targetElement = document.querySelector(targetId)
+
+         if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' })
+
+            history.replaceState(null, document.title, window.location.pathname + window.location.search)
+         }
+      }
+   });
 })
 
 export { headerLinkElements }
